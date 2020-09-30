980477c13cf5b4ac76887d5218c08784a982dbb9
patrickjiang <patrick.jiang86@gmail.com>
2020-09-30 11:41:12 -0400

    add serach function and dropdown list

--- app.js
+++ app.js
@@ -1,1 +1,75 @@
-console.log('test');
+//*****************************
+//Using fetch to get http request
+//******************************
+
+// fetch('http://www.omdbapi.com/?apikey=a86d7c66&s=avengers')
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data.Search);
+//   });
+
+const fetchData = async (searchTerm) => {
+  const response = await axios('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'a86d7c66',
+      s: searchTerm,
+    },
+  });
+
+  if (response.data.Error) {
+    return [];
+  }
+  return response.data.Search;
+};
+
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+  <lable><b>Search For a Movie</b></lable>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results">
+      </div>
+    </div>
+  </div>
+`;
+
+const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
+const onInput = async (e) => {
+  const movies = await fetchData(e.target.value);
+  if (!movies.length) {
+    dropdown.classList.remove('is-active');
+    return;
+  }
+
+  resultsWrapper.innerHTML = '';
+  dropdown.classList.add('is-active');
+  for (let moive of movies) {
+    const option = document.createElement('a');
+    const imgSrc = moive.Poster === 'N/A' ? '' : moive.Poster
+
+    option.classList.add('dropdown-item');
+    option.innerHTML = `
+      <img src="${imgSrc}" />
+      ${moive.Title}
+    `;
+    option.addEventListener('click', () => {
+      dropdown.classList.remove('is-active');
+      input.value = moive.Title;
+    })
+    resultsWrapper.appendChild(option);
+  }
+};
+
+input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', (e) => {
+  if (!root.contains(e.target)) {
+    dropdown.classList.remove('is-active');
+  }
+})
---
+++ utils.js
@@ -0,0 +1,14 @@
+//*****************************
+// Debounce function
+//******************************
+const debounce = (func, delay = 1000) => {
+  let timeOutId;
+  return (...args) => {
+    if (timeOutId) {
+      clearTimeout(timeOutId);
+    }
+    timeOutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
